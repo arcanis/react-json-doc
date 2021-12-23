b7f81b721a959e303dd7151b00a9104a769ecde6
Maël Nison <nison.mael@gmail.com>
2021-12-23 22:25:55 +0100

    Disables mjs

--- rollup.config.js
+++ rollup.config.js
@@ -4,11 +4,13 @@ import ts from '@rollup/plugin-typescript';
 export default {
   input: `./sources/index.tsx`,
   output: [
+/*
     {
       dir: `lib`,
       entryFileNames: `[name].mjs`,
       format: `es`,
     },
+*/
     {
       dir: `lib`,
       entryFileNames: `[name].js`,
